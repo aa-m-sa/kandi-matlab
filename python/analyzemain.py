ef0876cfe9e0a33a8197f3eff1175e860d8c8e8b
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2015-12-07 21:19:46 +0200

    rm analyze_particular, add plot producing (todo: printing)

--- python/analyzemain.py
+++ python/analyzemain.py
@@ -5,10 +5,13 @@
 Script. Analyze and plot all
 """
 
-import saiotools
-import sadistance
 import numpy as np
 import copy
+
+import saiotools
+import sadistance
+import saviz
+
 
 # selected: reserved globals
 
@@ -112,17 +115,43 @@ def analyze_all(scenario_list, basedirname):
             meta['scenario'] = s
             analyze_circleSet(circleSetData, circleSetResultData, targetData[numCircles], meta)
 
-def analyze_particular(scenario, basedirname, circleSet):
-    targetData = saiotools.load_set2_target()
-    #
+def run_print_plotters():
+    """
+    Get data an call print routines from saviz for plotting the prints.
+    """
+
+    t = saiotools.load_set2_target()
+
+    tImsAll, tCircsAll, tDescsAll = pick_selected_target_ims(t)
+    f1, ax1 = saviz.plot_datasets_2x3(tImsAll, tDescsAll)
+
+    tImsA, tCircsA, tDescsA = pick_selected_target_ims(t, sList=selectedTargetSetsA)
+
+    resA_slow = pick_nload_selected_all_results(sTargetList=selectedTargetSetsA, sDataResults=selectedDataResultsA_slow)
+    resA_fast = pick_nload_selected_all_results(sTargetList=selectedTargetSetsA, sDataResults=selectedDataResultsA_fast)
+
+    bestEnergiesA_slow, bestEnergyIndsA_slow, bestEnergyCirclesA_slow = pick_best_energy_results_A(resA_slow)
+    bestEnergiesA_fast, bestEnergyIndsA_fast, bestEnergyCirclesA_fast = pick_best_energy_results_A(resA_fast)
+
+    f2, ax2 = saviz.plot_3datasets_results(tImsA, tDescsA, bestEnergyCirclesA_slow, tCircsA)
+    f3, ax3 = saviz.plot_3datasets_results(tImsA, tDescsA, bestEnergyCirclesA_fast, tCircsA)
+
+
+    f4, ax4 = saviz.final_energies_histo3([e for e,r in resA_slow])
+    f5, ax5 = saviz.final_energies_histo3([e for e,r in resA_fast])
+
+    beErrors_slow = get_best_enery_error_rates(bestEnergyCirclesA_slow, tCircsA, measure=sadistance.naive_dist)
+    beErrors_fast = get_best_enery_error_rates(bestEnergyCirclesA_fast, tCircsA, measure=sadistance.naive_dist)
+
+
 
 if __name__ == "__main__":
-    scenario_list = ['t99-n1000',
-                     't98-n1000',
-                     't96-n600',
-                     't94-n600',
-                     't90-n600']
+    scenario_list_all = ['t99-n1000',
+                         't98-n1000',
+                         't96-n600',
+                         't94-n600',
+                         't90-n600']
 
     basedirname = '../testdata-annealingset2b-50x50-'
     #analyze_all(scenario_list, basedirname)
-    #analyze_particular('')
+    run_print_plotters()
